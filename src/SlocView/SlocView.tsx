d94af01d1181035779fd183f5503ad1ee6dfc659
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2020-05-17 17:18:31 +0200

    feat(slocview): highlight on mouse hover

--- src/SlocView/SlocView.tsx
+++ src/SlocView/SlocView.tsx
@@ -1,5 +1,6 @@
-import React, { SFC, useRef, useEffect } from 'react';
-import { select, pie, arc, interpolateHcl, DefaultArcObject, scaleOrdinal, quantize } from 'd3';
+import React, { SFC, useState } from 'react';
+import { pie, arc, interpolateHcl, DefaultArcObject, scaleOrdinal, quantize } from 'd3';
+import styled from 'styled-components';
 
 import { Project, File, ProjectItemType } from '../project/Project';
 
@@ -13,11 +14,15 @@ const height = 500;
 /**
  *
  */
-function slocViewArc(datum: Omit<DefaultArcObject, 'innerRadius' | 'outerRadius'>): string | null {
+function slocViewArc(
+  datum: Omit<DefaultArcObject, 'innerRadius' | 'outerRadius'>
+): string | undefined {
   const radius = Math.min(width, height) / 2;
   const innerRadius = radius * 0.67;
   const outerRadius = radius - 1;
-  return arc()({ innerRadius, outerRadius, ...datum });
+  const result = arc()({ innerRadius, outerRadius, ...datum });
+  if (!result) return undefined;
+  return result;
 }
 
 /**
@@ -31,11 +36,42 @@ const slocPie = pie<File>()
 /**
  *
  */
+const Button = styled.button`
+  /* Basics */
+  padding: 0;
+  border: none;
+  background-color: transparent;
+
+  /* Change the font styles in all browsers. */
+  font: inherit; /* 1 */
+  color: inherit;
+  line-height: 1.15; /* 1 */
+
+  /* Remove the margin in Firefox and Safari. */
+  margin: 0; /* 2 */
+
+  /* Show the overflow in Edge. */
+  overflow: visible;
+
+  /* Remove the inheritance of text transform in Firefox. */
+  text-transform: none;
+
+  /* Correct the inability to style clickable types in iOS and Safari. */
+  -webkit-appearance: button;
+`;
+
+/**
+ *
+ */
 const SlocView: SFC<SlocViewProps> = function SlocView(props) {
   const { data } = props;
+  const root = data;
 
-  // Get a reference to the svg node, for d3 to render stuff in
-  const svgNode = useRef(null);
+  // //
+  // const [selectedFileName, setSelectedFileName] = useState<string | null>(null);
+
+  //
+  const [hoveredFileName, setHoveredFileName] = useState<string | null>(null);
 
   // Bail if any of the project items is not a file
   const files = data.content.map((file) => {
@@ -46,50 +82,56 @@ const SlocView: SFC<SlocViewProps> = function SlocView(props) {
   });
 
   // Color from project item name
+  // https://observablehq.com/@d3/working-with-color?collection=@d3/d3-color
   // const colors = quantize(interpolateHcl('#f4e153', '#362142'), files.length);
   // const colors = quantize(interpolateHcl('#f4e153', '#6c3d8c'), files.length);
   const colors = quantize(interpolateHcl('#f4e153', '#6c3d8c'), files.length);
   const color = scaleOrdinal(colors).domain(files.map((file) => file.name));
 
-  /**
-   * Draw a donut
-   *
-   * Sources:
-   *
-   * - https://observablehq.com/@d3/donut-chart
-   * - https://observablehq.com/@d3/working-with-color?collection=@d3/d3-color
-   */
-  useEffect(() => {
-    // Bail if the node is null
-    if (!svgNode.current) return;
-
-    // Select the svg
-    const svg = select(svgNode.current).attr(
-      'viewBox',
-      [-width / 2, -height / 2, width, height].join(' ')
-    );
-
-    // Arcs from the files
-    const arcs = slocPie(files);
-
-    // Draw the donut
-    svg
-      .selectAll('path')
-      .data(arcs)
-      .join('path')
-      .attr('fill', (d) => color(d.data.name))
-      .attr('d', slocViewArc)
-      .append('title')
-      .text((d) => d.data.name);
-  }, [svgNode.current, data]);
+  const arcs = slocPie(files);
 
   return (
     <div>
-      <svg ref={svgNode} width={width} height={height} />
+      <svg
+        width={width}
+        height={height}
+        viewBox={`${-width / 2} ${-height / 2} ${width} ${height}`}
+      >
+        {arcs.map((fileArc) => (
+          <path
+            style={{ cursor: 'pointer' }}
+            key={fileArc.data.name}
+            fill={color(fileArc.data.name)}
+            d={slocViewArc(fileArc)}
+            // onClick={(): void => setSelectedFileName(fileArc.data.name)}
+            onMouseEnter={(): void => setHoveredFileName(fileArc.data.name)}
+            onMouseLeave={(): void => {
+              if (hoveredFileName === fileArc.data.name) setHoveredFileName(null);
+            }}
+          />
+        ))}
+      </svg>
+      <h4 style={{ color: 'white' }}>
+        <strong>{hoveredFileName || root.name}</strong>
+        {`: ${
+          hoveredFileName
+            ? files.filter((file) => file.name === hoveredFileName)[0].numberOfLines
+            : root.numberOfLines
+        }`}
+      </h4>
       {files.map((file) => (
-        <p key={file.name} style={{ color: color(file.name) }}>
-          <strong>{file.name}</strong>
-          {`: ${file.numberOfLines} lines`}
+        <p key={file.name}>
+          <Button
+            style={{
+              color: color(file.name),
+              cursor: 'pointer',
+              textDecoration: hoveredFileName === file.name ? 'underline' : 'none',
+            }}
+            // onClick={(): void => setHoveredFileName(file.name)}
+          >
+            <strong>{file.name}</strong>
+            {`: ${file.numberOfLines} lines`}
+          </Button>
         </p>
       ))}
     </div>
