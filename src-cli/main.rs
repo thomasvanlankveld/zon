7d7c602d31445e034656249c025edbdcbd75b41e
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2020-06-06 16:11:51 +0200

    feat(cli): count lines and serve ui from binary

--- src-cli/main.rs
+++ src-cli/main.rs
@@ -1,5 +1,26 @@
+use std::env;
+use std::error::Error;
+use tokei::{Config, Languages};
+use warp::Filter;
+
 #[tokio::main]
-async fn main() {
-    let zon_ui = warp::fs::dir("./dist/");
-    warp::serve(zon_ui).run(([127, 0, 0, 1], 3030)).await;
+async fn main() -> Result<(), Box<dyn Error>> {
+    let project_path = env::current_dir()?;
+
+    let mut languages = Languages::new();
+    let config = Config::default();
+    languages.get_statistics(&[project_path], &[], &config);
+
+    // println!("{}", serde_json::to_string(&languages)?);
+    let tokei_json = serde_json::to_string(&languages)?;
+
+    let served_zon_ui = warp::path("ui").and(warp::fs::dir("./dist/"));
+    let served_tokei_json = warp::path("input").map(move || format!("{}", tokei_json));
+
+    let served = served_zon_ui.or(served_tokei_json);
+    // let zon_ui = warp::fs::dir("./dist/");
+    warp::serve(served).run(([127, 0, 0, 1], 3030)).await;
+    // let input_file = warp::fs::file();
+    // warp::serve(input_file).run(([127, 0, 0, 1], 3031)).await;
+    Ok(())
 }
--- src-ui/App/App.tsx
+++ src-ui/App/App.tsx
@@ -1,16 +1,33 @@
-import React, { SFC } from 'react';
+import React, { SFC, useEffect, useState } from 'react';
 import SlocView from '../SlocView/SlocView';
 import tokeiAdapter from '../adapters/tokeiAdapter';
-import fromTokei from '../../input/fromTokei.json';
+// import fromTokei from '../../input/fromTokei.json';
+import { Project } from '../project/Project';
 
-// Get demo input data
-const data = tokeiAdapter(fromTokei, 'zon');
+// // Get demo input data
+// const data = tokeiAdapter(fromTokei, 'zon');
 
 /**
  *
  */
 const App: SFC = function App() {
-  return <SlocView data={data} />;
+  const [data, setData] = useState<Project | null>(null);
+
+  useEffect(() => {
+    if (data == null) {
+      fetch('http://localhost:3030/input')
+        .then(async (response) => {
+          const json = await response.json();
+          const parsed = tokeiAdapter(json, 'zon');
+          setData(parsed);
+        })
+        .catch((errr) => {
+          throw errr;
+        });
+    }
+  }, [data]);
+
+  return data == null ? <p>Loading...</p> : <SlocView data={data} />;
 };
 
 export default App;
