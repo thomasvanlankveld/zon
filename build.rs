d4ef54e1e4b2ab9441445714b11fc827557f34e8
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2020-06-07 00:16:14 +0200

    refactor(cli): clean up rust code

--- build.rs
+++ build.rs
@@ -4,29 +4,33 @@ use std::fs;
 use std::path::Path;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // Get destination paths
     let out_dir = env::var_os("OUT_DIR").unwrap();
-    // let dest_path = Path::new(&out_dir).join("hello.rs");
     let loader_dest_path = Path::new(&out_dir).join("loader.rs");
     let html_dest_path = Path::new(&out_dir).join("index.html");
     let js_dest_path = Path::new(&out_dir).join("index.js");
 
+    // Get html file contents
     let html_dist_filename = "dist/index.html";
     let html_u8 = fs::read(html_dist_filename)?;
     let html = String::from_utf8_lossy(&html_u8);
+
+    // Get js file contents
     let js_filename = html.split("<script src=\"/ui/").collect::<Vec<&str>>()[1]
         .split("\"></script>")
         .collect::<Vec<&str>>()[0];
     let js_dist_filename = format!("{}{}", "dist/", &js_filename);
     let js_u8 = fs::read(&js_dist_filename)?;
     let js = String::from_utf8_lossy(&js_u8);
+
+    // Replace hashed js filename with "index.js"
     let html_with_index_js = html.replace(&js_filename, "index.js");
 
-    fs::write(&html_dest_path, format!("{}", html_with_index_js)).unwrap();
-    fs::write(&js_dest_path, format!("{}", js)).unwrap();
+    // Write html and js files
+    fs::write(&html_dest_path, format!("{}", html_with_index_js))?;
+    fs::write(&js_dest_path, format!("{}", js))?;
 
-    // let html_with_escaped_double_quotes = html.replace("\"", "\\\"");
-    // let js_with_escaped_double_quotes = js.replace("\"", "\\\"").replace("\\", "\\\\");
-
+    // Write "loader" file
     fs::write(
         &loader_dest_path,
         format!(
@@ -41,31 +45,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             html_dest_path = html_dest_path,
             js_dest_path = js_dest_path,
         ),
-    )
-    .unwrap();
-    // fs::write(
-    //     &loader_dest_path,
-    //     format!(
-    //         "pub fn html() -> &'static str {{
-    //             include_str!({html_dest_path:?})
-    //         }}
+    )?;
 
-    //         pub fn js() -> &'static str {{
-    //             include_str!({js_dest_path:?})
-    //         }}
-
-    //         pub fn js_filename() -> &'static str {{
-    //             \"{js_filename}\"
-    //         }}
-    //     ",
-    //         html_dest_path = html_dest_path,
-    //         js_dest_path = js_dest_path,
-    //         js_filename = js_filename,
-    //     ),
-    // )
-    // .unwrap();
-
-    // println!("{:?}", env::var_os("OUT_DIR"));
-
+    // Done!
     Ok(())
 }
--- src-cli/main.rs
+++ src-cli/main.rs
@@ -1,11 +1,10 @@
 use std::env;
 use std::error::Error;
-// use std::fs;
-// use std::path::Path;
 
 use tokei::{Config, Languages};
 use warp::Filter;
 
+// Include html and js file content loader
 include!(concat!(env!("OUT_DIR"), "/loader.rs"));
 
 #[tokio::main]
@@ -19,33 +18,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     languages.get_statistics(&[project_path], &[], &config);
     let tokei_json = serde_json::to_string(&languages)?;
 
-    // println!("html: {}", html());
-    // println!("js: {}", js());
-    // println!("{:?}", std::env::var("OUT_DIR"));
-
-    // Get UI
-    // let out_dir = env::var_os("OUT_DIR").unwrap();
-    // let html_path = Path::new(&out_dir).join("index.html");
-    // let html_u8 = fs::read(html_path)?;
-    // let html = String::from_utf8(html_u8)?;
-
-    // let js_path = Path::new(&out_dir).join("index.js");
-    // let js_u8 = fs::read(js_path)?;
-    // let js = String::from_utf8(js_u8)?;
-    // let zon_ui_index_html = include_str!(std::env::var("OUT_DIR").join("index.html"));
-    // let zon_ui_index_js = js();
-
-    // // Get UI folder path
-    // let executable_path = std::env::current_exe()?;
-    // let relative_ui_folder_path = executable_path.join("../../../dist");
-    // let ui_folder_path = std::fs::canonicalize(relative_ui_folder_path)?;
-
-    // Serve UI and analysis
+    // Get html contents and headers
     let served_zon_ui_index_html = warp::path!("ui")
         .map(|| html())
         .with(warp::reply::with::header("content-type", "text/html"))
         .with(warp::reply::with::header("content-length", html().len()))
         .with(warp::reply::with::header("accept-ranges", "bytes"));
+
+    // Get js contents and headers
     let served_zon_ui_index_js = warp::path!("ui" / "index.js")
         .map(|| js())
         .with(warp::reply::with::header(
@@ -55,12 +35,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .with(warp::reply::with::header("content-length", js().len()))
         .with(warp::reply::with::header("accept-ranges", "bytes"));
 
-    // let served_zon_ui_index_html = warp::path!("ui").map(move || format!("{}", html));
-    // let served_zon_ui_index_js = warp::path!("ui" / "index.js").map(move || format!("{}", js));
+    // Serve UI and analysis json
     let served_zon_ui = served_zon_ui_index_html.or(served_zon_ui_index_js);
-
-    // let served_zon_ui = warp::path("ui").and(warp::fs::dir(ui_folder_path));
-
     let served_tokei_json = warp::path("input").map(move || format!("{}", tokei_json));
     let served = served_zon_ui.or(served_tokei_json);
     warp::serve(served).run(([127, 0, 0, 1], 3030)).await;
