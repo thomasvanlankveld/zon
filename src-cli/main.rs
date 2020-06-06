5d2d7c015fe07d232dbde238bda15d99c3383e34
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2020-06-06 16:58:50 +0200

    feat(cli): serve using correct UI folder from any location

--- src-cli/main.rs
+++ src-cli/main.rs
@@ -1,26 +1,31 @@
 use std::env;
 use std::error::Error;
+
 use tokei::{Config, Languages};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // Get path of the project to analyse
     let project_path = env::current_dir()?;
 
+    // Get analysis as json
     let mut languages = Languages::new();
     let config = Config::default();
     languages.get_statistics(&[project_path], &[], &config);
-
-    // println!("{}", serde_json::to_string(&languages)?);
     let tokei_json = serde_json::to_string(&languages)?;
 
-    let served_zon_ui = warp::path("ui").and(warp::fs::dir("./dist/"));
-    let served_tokei_json = warp::path("input").map(move || format!("{}", tokei_json));
+    // Get UI folder path
+    let executable_path = std::env::current_exe()?;
+    let relative_ui_folder_path = executable_path.join("../../../dist");
+    let ui_folder_path = std::fs::canonicalize(relative_ui_folder_path)?;
 
+    // Serve UI and analysis
+    let served_zon_ui = warp::path("ui").and(warp::fs::dir(ui_folder_path));
+    let served_tokei_json = warp::path("input").map(move || format!("{}", tokei_json));
     let served = served_zon_ui.or(served_tokei_json);
-    // let zon_ui = warp::fs::dir("./dist/");
     warp::serve(served).run(([127, 0, 0, 1], 3030)).await;
-    // let input_file = warp::fs::file();
-    // warp::serve(input_file).run(([127, 0, 0, 1], 3031)).await;
+
+    // Close
     Ok(())
 }
