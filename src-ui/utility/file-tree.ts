bb466824a5ac568c2555f69fd84bb59d47b6d8cc
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2020-06-28 15:57:36 +0200

    refactor(file-tree): extract helper functions from createTreeFromFiles()

--- src-ui/utility/file-tree.ts
+++ src-ui/utility/file-tree.ts
@@ -249,17 +249,37 @@ export function rootWithNode<FileData extends object = {}, FolderData extends ob
 }
 
 /**
+ * Whether all of the given files have the same path root
+ */
+function haveDifferentRoots(files: { path: string }[]): boolean {
+  return uniqBy(files, (file) => pathRoot(file.path)).length > 1;
+}
+
+/**
+ * Whether the given file exists at its path's root
+ *
+ * This is true when the file's path only has a single element
+ */
+function existAtRoot(file: { path: string }): boolean {
+  return toPathArray(file.path).length === 1;
+}
+
+/**
+ * Prepend an empty root to the given file
+ */
+function withEmptyRoot<File extends { path: string }>(file: File): File {
+  return { ...file, path: `/${file.path}` };
+}
+
+/**
  * Create a tree from an array of files
  */
 export function createTreeFromFiles<FileData extends object>(
   files: { path: string; data: FileData }[]
 ): FileSystemNode<FileData> {
-  // Add empty root to all paths if necessary
+  // Add empty root to all paths if necessary. This is the case under two circumstances. Firstly, if any of the files have different roots, we need to add a common shared root. Secondly, if any of the files exist at the root pathselves, we need to add a root, since the root must be a folder.
   const rootedFiles =
-    uniqBy(files, (file) => pathRoot(file.path)).length > 1 ||
-    files.some((file) => toPathArray(file.path).length < 2)
-      ? files.map((file) => ({ ...file, path: `/${file.path}` }))
-      : files;
+    haveDifferentRoots(files) || files.some(existAtRoot) ? files.map(withEmptyRoot) : files;
 
   // Get root name from the first path segment of the first file
   const rootName = pathRoot(rootedFiles[0].path);
