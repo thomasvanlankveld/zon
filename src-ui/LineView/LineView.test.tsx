73dbc07415113d7e0385c9d6c47e1b08cdcbea2a
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2020-12-29 13:12:13 +0100

    test(lineview): draft breadcrumb navigation test

--- src-ui/LineView/LineView.test.tsx
+++ src-ui/LineView/LineView.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import LineView from './LineView';
 import { createTreeFromFiles } from '../file-tree';
@@ -18,7 +18,7 @@ describe('LineView', () => {
     // When I render the line view
     const { getByRole } = render(<LineView data={project} />);
 
-    // Then I see the project name in the breadcrumbs
+    // Then I see the breadcrumbs for the project's root
     expect(getByRole('navigation', { name: /breadcrumbs/ })).toHaveTextContent('my-project');
     expect(getByRole('button', { name: 'my-project' })).toBeVisible();
 
@@ -32,7 +32,35 @@ describe('LineView', () => {
     expect(getByRole('button', { name: 'package.json : 30 lines' })).toBeVisible();
   });
 
-  it.todo('navigates to a folder on breadcrumb click');
+  // eslint-disable-next-line jest/no-disabled-tests
+  it.skip('navigates to a folder on breadcrumb click', () => {
+    expect.hasAssertions();
+
+    // Given a project with an `src` folder
+    const project = createTreeFromFiles([
+      { path: 'my-project/package.json', data: { numberOfLines: 30 } },
+      { path: 'my-project/src/foo.ts', data: { numberOfLines: 50 } },
+      { path: 'my-project/src/bar.ts', data: { numberOfLines: 20 } },
+    ]);
+
+    // And a rendered line view
+    const { getByRole } = render(<LineView data={project} />);
+
+    // And I navigated to the `src` folder in navigation
+    fireEvent.click(getByRole('button', { name: 'src : 70 lines' }));
+
+    // When I click the `my-project` breadcrumb in the navigation
+    fireEvent.click(getByRole('button', { name: 'my-project' }));
+
+    // Then I see the breadcrumbs for the project root
+    expect(false).toBe(true);
+
+    // And I see a visualization from the project root
+    expect(false).toBe(true);
+
+    // And I see a list of the project's top folders and files
+    expect(getByRole('heading', { name: 'my-project : 100 lines' })).toBeVisible();
+  });
   it.todo('navigates to a folder on diagram segment click');
   it.todo('navigates to the parent folder on diagram center click');
   it.todo('previews a folder or file on diagram segment hover (navigation and list)');
