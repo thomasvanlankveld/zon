4e2581369f8b420eac0e961b64ae269c9ea5a092
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2021-02-14 16:39:08 +0100

    feat(file-tree): make toPathArray drop the last empty segment for paths ending in slash

--- src-ui/file-tree/toPathArray.ts
+++ src-ui/file-tree/toPathArray.ts
@@ -4,5 +4,12 @@ import { PathString, PathArray } from './file-tree';
  * Convert path string to array of path segments
  */
 export default function toPathArray(pathString: PathString): PathArray {
-  return pathString.split('/');
+  // Split by slash
+  const segments = pathString.split('/');
+
+  // If the last segment is empty, drop it (for example: "ends/with/slash/")
+  if (segments[segments.length - 1] === '') return segments.slice(0, segments.length - 1);
+
+  // Return array of path segments
+  return segments;
 }
