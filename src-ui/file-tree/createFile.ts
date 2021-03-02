cffc42e730a2b5c090bb078061411f5755b24812
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2021-03-02 17:50:20 +0100

    feat(file-tree): accepth Path instead of string in createFile and createFolder

--- src-ui/file-tree/createFile.ts
+++ src-ui/file-tree/createFile.ts
@@ -1,23 +1,21 @@
-import { File, FileSystemNodeType } from './file-tree';
+import asPathString from './asPathString';
+import { File, FileSystemNodeType, Path } from './file-tree';
 import pathTip from './pathTip';
 
 /**
  * Create file node
  */
-export function createFile(path: string): File;
-export function createFile(path: string, fileData: undefined): File;
+export function createFile(path: Path): File;
+export function createFile(path: Path, fileData: undefined): File;
+export function createFile<FileData extends object>(path: Path, fileData: FileData): File<FileData>;
 export function createFile<FileData extends object>(
-  path: string,
-  fileData: FileData
-): File<FileData>;
-export function createFile<FileData extends object>(
-  path: string,
+  path: Path,
   fileData?: FileData
 ): File<FileData | {}> {
   return {
     type: FileSystemNodeType.File,
     nodeName: pathTip(path),
-    path,
+    path: asPathString(path),
     data: fileData || {},
   };
 }
--- src-ui/file-tree/createFolder.ts
+++ src-ui/file-tree/createFolder.ts
@@ -1,36 +1,37 @@
-import { Folder, FileSystemNode, FileSystemNodeType } from './file-tree';
+import asPathString from './asPathString';
+import { Folder, FileSystemNode, FileSystemNodeType, Path } from './file-tree';
 import pathTip from './pathTip';
 
 /**
  * Create folder node
  */
-export function createFolder(path: string): Folder;
-export function createFolder<FileData extends object>(path: string): Folder<FileData>;
+export function createFolder(path: Path): Folder;
+export function createFolder<FileData extends object>(path: Path): Folder<FileData>;
 export function createFolder<FolderData extends object>(
-  path: string,
+  path: Path,
   options: { folderData: FolderData }
 ): Folder<{}, FolderData>;
 export function createFolder<FileData extends object, FolderData extends object>(
-  path: string,
+  path: Path,
   options: { folderData: FolderData }
 ): Folder<FileData, FolderData>;
 export function createFolder<FileData extends object, ChildrenFolderData extends object = {}>(
-  path: string,
+  path: Path,
   options: { children: FileSystemNode<FileData, {}, {}, ChildrenFolderData>[] }
 ): Folder<FileData, {}, ChildrenFolderData>;
 export function createFolder<FileData extends object, FolderData extends object>(
-  path: string,
+  path: Path,
   options: { folderData: FolderData; children: FileSystemNode<FileData, FolderData>[] }
 ): Folder<FileData, FolderData>;
 export function createFolder<FileData extends object, FolderData extends object>(
-  path: string,
+  path: Path,
   options: { folderData?: FolderData; children?: FileSystemNode<FileData, FolderData>[] } = {}
 ): Folder<FileData | {}, FolderData | {}> {
   const { folderData, children } = options;
   return {
     type: FileSystemNodeType.Folder,
     nodeName: pathTip(path),
-    path,
+    path: asPathString(path),
     data: folderData || {},
     children: children || [],
   };
