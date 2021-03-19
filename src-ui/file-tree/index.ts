03544c4992c4840c8520ff59376bc712358451ae
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2021-03-19 22:23:58 +0100

    feat(file-tree): expose newest utilities

--- src-ui/file-tree/index.ts
+++ src-ui/file-tree/index.ts
@@ -1,10 +1,14 @@
 import asPathArray from './asPathArray';
+import asPathString from './asPathString';
 import getFileByPath from './getFileByPath';
 import getFolderByPath from './getFolderByPath';
 import getNodeByPath from './getNodeByPath';
+import getNodesAlongPath from './getNodesAlongPath';
 import isFile from './isFile';
 import isFolder from './isFolder';
 import mergeTrees from './mergeTrees';
+import NodeNotFoundError from './NodeNotFoundError';
+import pathParent from './pathParent';
 import pathRoot from './pathRoot';
 import pathTip from './pathTip';
 import sortNodes from './sortNodes';
@@ -20,12 +24,16 @@ export * from './mapNodes';
 export * from './reduceNodes';
 export {
   asPathArray,
+  asPathString,
   getFileByPath,
   getFolderByPath,
   getNodeByPath,
+  getNodesAlongPath,
   isFile,
   isFolder,
   mergeTrees,
+  NodeNotFoundError,
+  pathParent,
   pathRoot,
   pathTip,
   sortNodes,
