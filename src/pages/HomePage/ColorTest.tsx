07c59062eb96a6867260b63c9e582653b52868e0
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2025-01-30 16:43:00 +0100

    221: remove unneeded fragment

--- src/pages/HomePage/ColorTest.tsx
+++ src/pages/HomePage/ColorTest.tsx
@@ -20,12 +20,10 @@ function ColorSpread(props: { getColor: (val: number) => string }) {
 
 export default function ColorTest() {
   return (
-    <>
-      <div style={{ display: "grid", padding: "6rem" }}>
-        <ColorSpread getColor={(val) => rainbow(val).regular} />
-        <ColorSpread getColor={(val) => rainbow(val).deemphasize} />
-        <ColorSpread getColor={(val) => rainbow(val).active} />
-      </div>
-    </>
+    <div style={{ display: "grid", padding: "6rem" }}>
+      <ColorSpread getColor={(val) => rainbow(val).regular} />
+      <ColorSpread getColor={(val) => rainbow(val).deemphasize} />
+      <ColorSpread getColor={(val) => rainbow(val).active} />
+    </div>
   );
 }
