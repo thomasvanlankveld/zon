e9e67ffce2ea9a6e1b543ed401c9d8b5918dcfc6
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2025-02-17 16:56:00 +0100

    185: fix report list so that the heading and the tab list are fixed when scrolling the list

--- src/pages/ReportPage/ReportList/ReportList.tsx
+++ src/pages/ReportPage/ReportList/ReportList.tsx
@@ -36,45 +36,41 @@ export default function ReportList() {
   //     .map((_, i) => rainbow(getPosition(i)).regular);
   // const linearGradient = () => `linear-gradient(${colors().join(", ")})`;
 
+  // TODO: Maybe this shouldn't be a nav? Check https://a11y-style-guide.com/style-guide/section-navigation.html
   return (
-    <div class="overflow-x-hidden">
-      {/* <div> */}
-      {/* TODO: Maybe this shouldn't be a nav? Check https://a11y-style-guide.com/style-guide/section-navigation.html */}
-      {/* <div
-        style={{
-          "--glow-background": linearGradient(),
-          "--glow-opacity": "0.2",
-        }}
-        class="glow"
-      > */}
-      <nav
-        // TODO: Fix report-list__list being used in two places
-        style={{
-          "border-radius": "var(--spacing-xxl)",
-          "padding-block": "var(--spacing-xxl)",
-          "padding-inline": "var(--spacing-xxl)",
-          // "padding-block": "var(--spacing-xl)",
-          // "padding-inline": "var(--spacing-xl)",
-          "background-color": "var(--color-background)",
-          gap: "var(--spacing-xs)",
-        }}
-        class={`${styles["report-list__list"]} overflow-x-hidden`}
-        aria-label={t("report-list.nav.label", {
-          name: getDisplayName(listRoot().name, t("group-name")),
-        })}
-      >
-        {/* <div style={{ display: "grid", padding: "var(--spacing-xs)" }}> */}
-        <ListHeading />
-        {/* </div> */}
-        <ReportTabList
-          selectedTab={selectedTab()}
-          setSelectedTab={setSelectedTab}
-        />
-        <ContentPanel selectedTab={selectedTab()} />
-        <LineTypePanel selectedTab={selectedTab()} />
-        <LanguagePanel selectedTab={selectedTab()} />
-      </nav>
+    <nav
+      // TODO: Fix report-list__tab-panel being used in two places
+      style={{
+        // TODO: Inject two lines below from report
+        "align-self": "start",
+        "max-height": "100%",
+        // TODO: Inject two lines above from report
+
+        "border-radius": "var(--spacing-xxl)",
+        "padding-block": "var(--spacing-xxl)",
+        "padding-inline": "var(--spacing-xxl)",
+        // "padding-block": "var(--spacing-xl)",
+        // "padding-inline": "var(--spacing-xl)",
+        "background-color": "var(--color-background)",
+        display: "grid",
+        "grid-template-rows": "min-content min-content 1fr",
+        gap: "var(--spacing-xs)",
+      }}
+      class={`${styles["report-list__card"]} overflow-x-hidden`}
+      aria-label={t("report-list.nav.label", {
+        name: getDisplayName(listRoot().name, t("group-name")),
+      })}
+    >
+      {/* <div style={{ display: "grid", padding: "var(--spacing-xs)" }}> */}
+      <ListHeading />
       {/* </div> */}
-    </div>
+      <ReportTabList
+        selectedTab={selectedTab()}
+        setSelectedTab={setSelectedTab}
+      />
+      <ContentPanel selectedTab={selectedTab()} />
+      <LineTypePanel selectedTab={selectedTab()} />
+      <LanguagePanel selectedTab={selectedTab()} />
+    </nav>
   );
 }
