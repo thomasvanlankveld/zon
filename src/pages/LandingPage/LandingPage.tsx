9540136f9f99321551f799b30ed836e937ff9b7b
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2025-02-19 14:08:39 +0100

    185: merge landing page glimmer and glow into the card div

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -37,61 +37,51 @@ export default function LandingPage(props: LandingPageProps) {
       <BackgroundConfig opacity={0.01} />
       <div
         style={{
-          flex: "1 1 auto",
-          // TODO: Fix glow wrapper and content height into a single variable
+          "--glimmer-border-radius": "var(--spacing-xl)",
           height: "min(var(--container-s), 100%)",
           width: "min(var(--container-l), 100%)",
-          "--glimmer-border-radius": "var(--spacing-xl)",
+          display: "flex",
+          "flex-direction": "column",
+          "align-items": "center",
+          "justify-content": "space-evenly",
         }}
-        class="glimmer glow"
+        class="card glimmer glow"
       >
+        <h1
+          class="heading-xxl"
+          style={{
+            display: "flex",
+            gap: "var(--spacing-xl)",
+            "align-items": "center",
+          }}
+        >
+          <Logo size={70} />
+          {t("app.title")}
+        </h1>
+
         <div
           style={{
-            height: "min(var(--container-s), 100%)",
-            overflow: "auto",
-            display: "flex",
-            "flex-direction": "column",
-            "align-items": "center",
-            "justify-content": "space-evenly",
+            display: "grid",
+            "place-items": "center",
+            gap: "var(--spacing-xl)",
+            "text-align": "center",
+            "text-wrap": "balance",
           }}
-          class="card"
         >
-          <h1
-            class="heading-xxl"
-            style={{
-              display: "flex",
-              gap: "var(--spacing-xl)",
-              "align-items": "center",
-            }}
+          <span
+            style={{ "min-height": "calc(2 * var(--line-height-regular))" }}
           >
-            <Logo size={70} />
-            {t("app.title")}
-          </h1>
-
-          <div
-            style={{
-              display: "grid",
-              "place-items": "center",
-              gap: "var(--spacing-xl)",
-              "text-align": "center",
-              "text-wrap": "balance",
-            }}
-          >
-            <span
-              style={{ "min-height": "calc(2 * var(--line-height-regular))" }}
+            <Show
+              when={props.countingPath}
+              fallback={t("landing-page.welcome-message")}
             >
-              <Show
-                when={props.countingPath}
-                fallback={t("landing-page.welcome-message")}
-              >
-                {(definedPath) => <CountingLines path={definedPath()} />}
-              </Show>
-            </span>
-            <div>
-              <UploadButton
-                countLinesInFolder={logAsyncErrors(countLinesInFolder)}
-              />
-            </div>
+              {(definedPath) => <CountingLines path={definedPath()} />}
+            </Show>
+          </span>
+          <div>
+            <UploadButton
+              countLinesInFolder={logAsyncErrors(countLinesInFolder)}
+            />
           </div>
         </div>
       </div>
