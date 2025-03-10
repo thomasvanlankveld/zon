9ed6335b4eda15755070a4b585ec3652610162b3
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2025-03-10 18:19:20 +0100

    252: improve comment readability

--- vite.config.ts
+++ vite.config.ts
@@ -46,7 +46,7 @@ export default defineConfig(() => ({
   build: {
     // Disable minification to prevent Vite from crashing on the Terser plugin because it's trying to use require
     minify: false,
-    // Commented out target because these will be overridden by @vitejs/plugin-legacy
+    // Commented out `target` because these will be overridden by @vitejs/plugin-legacy
     // target: ["es2021", "safari13"],
   },
 }));
