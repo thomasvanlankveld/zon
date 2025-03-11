e74df13a1a78baa1e2d2ca487c34ae5bdbf560f4
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2025-03-11 14:31:33 +0100

    257: remove glimmer animation

--- src/components/ShinyButton/ShinyButton.tsx
+++ src/components/ShinyButton/ShinyButton.tsx
@@ -10,7 +10,6 @@ type ShinyButtonProps = {
 export default function ShinyButton(props: ShinyButtonProps) {
   return (
     <button
-      style={{ "--glimmer-border-radius": "8px" }}
       class={`${styles["shiny-button"]} glimmer glimmer-hover glow`}
       type={props.type ?? "button"}
       onClick={() => props.onClick?.()}
