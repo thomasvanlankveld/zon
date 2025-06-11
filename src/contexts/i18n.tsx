f725af701a69e34ef35ca824fa49fdd449b3db48
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2025-06-11 20:18:16 +0200

    174: add some docstrings to the i18n context

--- src/contexts/i18n.tsx
+++ src/contexts/i18n.tsx
@@ -12,9 +12,23 @@ const mapping: Record<Locale, Translations> = {
   "en-GB": enGB,
 };
 
+/**
+ * Create the internationalization services.
+ *
+ * Static types use the `en-GB` locale as a base.
+ *
+ * @returns {I18n} The internationalization services
+ */
 function createI18n() {
   const [locale, setLocale] = createSignal<Locale>("en-GB");
 
+  /**
+   * Translate a string.
+   *
+   * @param {TranslationKey} key - The key of the string to translate
+   * @param {TranslationArgs[K]} [args] - The arguments to pass to the translation function
+   * @returns {string} The translated string
+   */
   function t<K extends TranslationKey>(
     key: K,
     args?: TranslationArgs[K],
@@ -40,6 +54,12 @@ function createI18n() {
     return entry;
   }
 
+  /**
+   * Format a number.
+   *
+   * @param {number} number - The number to format
+   * @returns {string} The formatted number
+   */
   function formatNumber(number: number) {
     return new Intl.NumberFormat(locale()).format(number);
   }
@@ -49,6 +69,9 @@ function createI18n() {
 
 const I18nContext = createContext();
 
+/**
+ * Provide internationalization services.
+ */
 export function I18nProvider(props: { children: JSX.Element }) {
   const i18n = createI18n();
 
@@ -57,8 +80,21 @@ export function I18nProvider(props: { children: JSX.Element }) {
   );
 }
 
+/**
+ * Internationalization services.
+ *
+ * @property {(key: TranslationKey, args?: TranslationArgs[K]) => string} t - Translate a string
+ * @property {() => string} locale - Get the current locale
+ * @property {(locale: Locale) => void} setLocale - Set the current locale
+ * @property {(number: number) => string} formatNumber - Format a number
+ */
 type I18n = ReturnType<typeof createI18n>;
 
+/**
+ * Get the internationalization services.
+ *
+ * @returns {I18n} The internationalization services
+ */
 export function useI18n(): I18n {
   const context = useContext(I18nContext);
   if (!context) {
