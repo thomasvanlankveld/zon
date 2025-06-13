6fc64f3fa50d235f8f059f66d1051a684b6acaf4
Thomas van Lankveld <5240626+thomasvanlankveld@users.noreply.github.com>
2025-06-13 16:48:22 +0200

    174: add explanatory comment to MaybeUpdater

--- src/components/Updater/MaybeUpdater.tsx
+++ src/components/Updater/MaybeUpdater.tsx
@@ -2,6 +2,11 @@ import { Show } from "solid-js";
 import { TARGET, useMeta } from "../../contexts/meta";
 import Updater from "./Updater";
 
+/**
+ * Wraps the Updater component, and  only renders it if the target is desktop.
+ *
+ * This way, the app does not crash because we're calling APIs that only work on desktop.
+ */
 export default function MaybeUpdater() {
   const meta = useMeta();
 
